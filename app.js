83c115c5486f33c2d3088cec914b55e9a9e75f08
Prathik Ganiga <mgprathik27@gmail.com>
2019-07-23 16:18:57 -0500

    Created server at por 3000

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 express = require('express');
+var app = express();
+const port = 3000;
+
+app.get('/',(req,res)=>{
+	res.send("hey there");
+});
+
+app.listen(port,()=>{
+	console.log("Server started at port "+port);
+} );
